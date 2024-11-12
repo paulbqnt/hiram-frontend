a285083d2495bb160bebce84e1584ac28b70050f
paulbqnt <paulbqnt@users.noreply.github.com>
2024-11-12 17:16:24 +0100

    chore: installed MUI and add theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import theme from "./theme";
 
 function App() {
   return (
-    <div className="App">
+      <div className="App">
 
-    </div>
+      </div>
   );
 }
 
---
+++ src/theme.ts
@@ -0,0 +1,13 @@
+import { createTheme } from '@mui/material';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#1760a5',
+            light: 'skyblue',
+        },
+        secondary: { main: '#15c630' },
+    },
+});
+
+export default theme;
